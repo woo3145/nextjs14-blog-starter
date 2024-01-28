bf0ac53c9fd81c7a12f5db18420e1ab612a9f823
woo3145 <lcwoo3145@gmail.com>
2024-01-29 07:16:46 +0900

    ✨ 다크 모드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './styles/globals.css';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { DarkModeToggle } from '@/components/darkmode-toggle';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -59,8 +61,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <DarkModeToggle />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
