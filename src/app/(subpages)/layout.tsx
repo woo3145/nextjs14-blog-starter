dcdd72eeeeee005e81744168de4d06161113858f
woo3145 <lcwoo3145@gmail.com>
2024-01-29 04:59:17 +0900

    ✨ 메타데이터 추가

--- src/app/(subpages)/layout.tsx
+++ src/app/(subpages)/layout.tsx
@@ -1,5 +1,12 @@
 import Header from '@/lib/components/header';
 
+export const metadata = {
+  title: 'Posts',
+  alternates: {
+    canonical: 'https://mysite.com/posts',
+  },
+};
+
 export default function SubLayout({
   children,
 }: Readonly<{
