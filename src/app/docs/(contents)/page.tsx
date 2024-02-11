b2e1323b1a68be3545a7db39290945d44bf61de8
woo3145 <lcwoo3145@gmail.com>
2024-02-12 02:34:50 +0900

    ✨ Docs - TracingBeam 추가

--- src/app/docs/(contents)/page.tsx
+++ src/app/docs/(contents)/page.tsx
@@ -2,6 +2,8 @@ import { Bread } from '../_components/bread';
 import { getDoc } from '@/lib/get-docs';
 import { MdxBody } from '@/components/mdx/mdx-body';
 import { notFound } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { TracingBeam } from '@/components/ui/tracking-beam';
 import '@/app/styles/mdx.css';
 
 export default async function IntroducePage() {
@@ -11,11 +13,19 @@ export default async function IntroducePage() {
     <div className="w-full flex flex-col md:px-6">
       <Bread items={['Docs', 'Introduce']} />
 
-      <div className="">
+      <TracingBeam className="">
         <div className="w-full">
+          <div className="space-y-2">
+            <h1 className={cn('scroll-m-20 text-4xl font-bold tracking-tight')}>
+              {doc.title}
+            </h1>
+            {doc.description && (
+              <p className="text-lg text-muted-foreground">{doc.description}</p>
+            )}
+          </div>
           <MdxBody>{doc.body}</MdxBody>
         </div>
-      </div>
+      </TracingBeam>
     </div>
   );
 }
