1206a2f76eb11bce9bf395942fa0664552c31918
woo3145 <lcwoo3145@gmail.com>
2024-02-11 16:08:55 +0900

    ✨ Skill Data Provider 툴팁기능 추가

--- src/app/(landing)/_components/skill-data-provider.tsx
+++ src/app/(landing)/_components/skill-data-provider.tsx
@@ -1,6 +1,7 @@
-import { motion, Variant, Variants, animate, useInView } from 'framer-motion';
+'use client';
+import { motion, Variants, useInView } from 'framer-motion';
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 interface Props {
   src: string;
@@ -8,6 +9,8 @@ interface Props {
   width: number;
   height: number;
   index: number;
+  title: string;
+  description?: string;
 }
 
 export const SkillDataProvider = ({
@@ -16,15 +19,24 @@ export const SkillDataProvider = ({
   width,
   height,
   index,
+  title,
+  description,
 }: Props) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
+  const [isHovered, setIsHovered] = useState(false);
 
+  // 이미지 애니메이션
   const imageVariants: Variants = {
     hidden: { opacity: 0 },
     animate: { opacity: 1 },
   };
 
+  // 툴팁 애니메이션
+  const tooltipVariants = {
+    hidden: { opacity: 0, y: -10, scale: 0.95 },
+    visible: { opacity: 1, y: 0, scale: 1 },
+  };
   const animationDealy = 0.3;
   return (
     <motion.div
@@ -34,6 +46,9 @@ export const SkillDataProvider = ({
       animate={isInView ? 'animate' : 'hidden'}
       custom={index}
       transition={{ delay: index * animationDealy }}
+      onHoverStart={() => setIsHovered(true)}
+      onHoverEnd={() => setIsHovered(false)}
+      className="relative flex justify-center"
     >
       {srcDark ? (
         <>
@@ -60,6 +75,20 @@ export const SkillDataProvider = ({
           alt="skill image"
           className="fill-foreground"
         />
+      )}
+      {isHovered && (
+        <motion.div
+          className="absolute z-40 -top-12 p-2 bg-background text-foreground border border-border rounded-md text-sm whitespace-pre"
+          variants={tooltipVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <strong>{title}</strong>
+          {description ? (
+            <p className="text-foreground/70">{description}</p>
+          ) : null}
+        </motion.div>
       )}
     </motion.div>
   );
--- src/app/(landing)/_components/skill-section.tsx
+++ src/app/(landing)/_components/skill-section.tsx
@@ -7,23 +7,70 @@ interface Skill {
   darkImageSrc?: string;
   width: number;
   height: number;
+  title: string;
+  description?: string;
 }
 
 const frontSkills: Skill[] = [
-  { imageSrc: '/skills/node-js.png', width: 100, height: 100 },
-  { imageSrc: '/skills/js.png', width: 100, height: 100 },
-  { imageSrc: '/skills/ts.png', width: 100, height: 100 },
-  { imageSrc: '/skills/react-icon.png', width: 100, height: 100 },
-  { imageSrc: '/skills/react-query.svg', width: 100, height: 100 },
+  { imageSrc: '/skills/node-js.png', width: 100, height: 100, title: 'nodejs' },
+  { imageSrc: '/skills/js.png', width: 100, height: 100, title: 'javascript' },
+  {
+    imageSrc: '/skills/ts.png',
+    width: 100,
+    height: 100,
+    title: 'typescript',
+    description: 'js로 못돌아가겠어..',
+  },
+  {
+    imageSrc: '/skills/react-icon.png',
+    width: 100,
+    height: 100,
+    title: 'react',
+    description: '만들어 주신분.. 감사합니다',
+  },
+  {
+    imageSrc: '/skills/react-query.svg',
+    width: 100,
+    height: 100,
+    title: 'react-query',
+    description: '덕분에 리덕스 탈출 1',
+  },
+  {
+    imageSrc: '/skills/zustand.png',
+    width: 100,
+    height: 100,
+    title: 'zustand',
+    description:
+      '덕분에 리덕스 탈출 2\n외에도 다양한 상태관리 라이브러리 학습가능',
+  },
   {
     imageSrc: '/skills/nextjs.svg',
     darkImageSrc: '/skills/nextjs-dark.svg',
     width: 100,
     height: 100,
+    title: 'nextjs',
+    description: 'nextjs 블로그 읽는게 취미',
   },
-  { imageSrc: '/skills/tailwind-css.svg', width: 100, height: 100 },
-  { imageSrc: '/skills/shadcn-ui.png', width: 100, height: 100 },
-  { imageSrc: '/skills/socket-io.svg', width: 100, height: 100 },
+  {
+    imageSrc: '/skills/tailwind-css.svg',
+    width: 100,
+    height: 100,
+    title: 'tailwindCSS',
+    description: '사용시 작업속도 2배 향상',
+  },
+  {
+    imageSrc: '/skills/shadcn-ui.png',
+    width: 100,
+    height: 100,
+    title: 'shadcn/ui',
+    description: '사용시 작업속도 2배 향상(2)',
+  },
+  {
+    imageSrc: '/skills/socket-io.svg',
+    width: 100,
+    height: 100,
+    title: 'socket.io',
+  },
 ];
 const backSkills: Skill[] = [
   {
@@ -31,24 +78,81 @@ const backSkills: Skill[] = [
     darkImageSrc: '/skills/express-dark.svg',
     width: 100,
     height: 100,
+    title: 'expressjs',
   },
-  { imageSrc: '/skills/nestjs.svg', width: 100, height: 100 },
-  { imageSrc: '/skills/mysql.png', width: 100, height: 100 },
-  { imageSrc: '/skills/postgre.png', width: 100, height: 100 },
-  { imageSrc: '/skills/mongodb.png', width: 100, height: 100 },
-  { imageSrc: '/skills/nginx.png', width: 100, height: 100 },
-  { imageSrc: '/skills/firebase.png', width: 100, height: 100 },
+  {
+    imageSrc: '/skills/nestjs.svg',
+    width: 100,
+    height: 100,
+    title: 'nestjs',
+    description: 'express보다 선호',
+  },
+  { imageSrc: '/skills/mysql.png', width: 100, height: 100, title: 'mysql' },
+  {
+    imageSrc: '/skills/postgre.png',
+    width: 100,
+    height: 100,
+    title: 'postgresql',
+  },
+  {
+    imageSrc: '/skills/mongodb.png',
+    width: 100,
+    height: 100,
+    title: 'mongodb',
+  },
+  {
+    imageSrc: '/skills/nginx.png',
+    width: 100,
+    height: 100,
+    title: 'nginx',
+    description: '정적 페이지, 프록시 서버 구축',
+  },
+  {
+    imageSrc: '/skills/firebase.png',
+    width: 100,
+    height: 100,
+    title: 'firebase',
+    description: '백엔드 짜기 귀찮을때 1',
+  },
+  {
+    imageSrc: '/skills/supabase.png',
+    width: 100,
+    height: 100,
+    title: 'supabase',
+    description: '백엔드 짜기 귀찮을때 2',
+  },
 ];
 const otherSkills: Skill[] = [
-  { imageSrc: '/skills/aws.png', width: 100, height: 100 },
-  { imageSrc: '/skills/docker.png', width: 100, height: 100 },
+  {
+    imageSrc: '/skills/aws.png',
+    width: 100,
+    height: 100,
+    title: 'aws',
+    description:
+      'EC2, S3, ElastiCache, RDS, CloudFront, Lambda 활용 가능\n상황에 맞춰 조사하고 활용하는편',
+  },
+  {
+    imageSrc: '/skills/docker.png',
+    width: 100,
+    height: 100,
+    title: 'docker',
+    description: 'docker, docker-compose 까진 가능',
+  },
   {
     imageSrc: '/skills/vercel.svg',
     darkImageSrc: '/skills/vercel-dark.svg',
     width: 100,
     height: 100,
+    title: 'vercel',
+    description: '배포 딸깍...',
   },
-  { imageSrc: '/문어_2.png', width: 100, height: 100 },
+  {
+    imageSrc: '/문어_2.png',
+    width: 100,
+    height: 100,
+    title: '하찮은 그림실력',
+    description: '개발에 필요하다면 다방면으로 학습/활용',
+  },
 ];
 
 export const SkillSection = () => {
@@ -67,6 +171,8 @@ export const SkillSection = () => {
                   width={skill.width}
                   height={skill.height}
                   index={idx}
+                  title={skill.title}
+                  description={skill.description}
                 />
               );
             })}
@@ -81,6 +187,8 @@ export const SkillSection = () => {
                   width={skill.width}
                   height={skill.height}
                   index={idx}
+                  title={skill.title}
+                  description={skill.description}
                 />
               );
             })}
@@ -95,6 +203,8 @@ export const SkillSection = () => {
                   width={skill.width}
                   height={skill.height}
                   index={idx}
+                  title={skill.title}
+                  description={skill.description}
                 />
               );
             })}
