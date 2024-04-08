4b2d2d9028f8f3779a7931555d8f82b5cdc9429e
woo3145 <lcwoo3145@gmail.com>
2024-04-09 04:46:08 +0900

    Add velite.config.ts for mdx types

---
+++ velite.config.ts
@@ -0,0 +1,90 @@
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import { defineCollection, defineConfig, s } from 'velite';
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split('/').slice(1).join('/'),
+});
+
+const docs = defineCollection({
+  name: 'Doc',
+  pattern: 'docs/**/*.mdx',
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      published: s.boolean().default(true),
+      toc: s.toc(),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+const posts = defineCollection({
+  name: 'Post',
+  pattern: 'blog/**/*.mdx',
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      image: s.string().max(99),
+      tags: s.array(s.string()),
+      authors: s.array(s.string()),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+const authors = defineCollection({
+  name: 'Author',
+  pattern: 'authors/**/*.mdx',
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      avatar: s.string().max(99),
+      github: s.string().max(99),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+export default defineConfig({
+  root: 'content',
+  output: {
+    data: '.velite',
+    assets: 'public/static',
+    base: '/static/',
+    name: '[name]-[hash:6].[ext]',
+    clean: true,
+  },
+  collections: { authors, posts, docs },
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: 'slack-dark',
+          keepBackground: true,
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ['anchor'],
+            ariaLabel: 'Link to section',
+          },
+        },
+      ],
+    ],
+  },
+});
