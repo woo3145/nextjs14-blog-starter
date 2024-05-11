8af90f28645cce921100bef065b282e444c817b4
woo3145 <lcwoo3145@gmail.com>
2024-05-11 21:16:20 +0900

    Fix build error

--- src/app/(landing)/_components/project-section/project-section.tsx
+++ src/app/(landing)/_components/project-section/project-section.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { ProjectText } from './project-text';
 import { projects as allProjects } from '#site/content';
-import { compareDesc } from 'date-fns';
 import { ProjectList } from './project-list';
 
 interface ProjectSectionProps {
@@ -15,9 +14,7 @@ export const ProjectSection = ({ className }: ProjectSectionProps) => {
   const projects = allProjects
     .filter((project) => project.published)
     .filter((project) => project.featured)
-    .sort((a, b) => {
-      return compareDesc(new Date(a.date), new Date(b.date));
-    });
+    .sort((a, b) => b.id - a.id);
 
   return (
     <section className={cn('min-h-screen w-full space-y-10', className)}>
