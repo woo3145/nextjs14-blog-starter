a4a99308cc86e66a6494a43c8e07c3152c138c4b
woo3145 <lcwoo3145@gmail.com>
2024-05-13 15:35:39 +0900

    Fix blog/project header for mobile devices

--- src/app/blog/[...slug]/_components/blog-header.tsx
+++ src/app/blog/[...slug]/_components/blog-header.tsx
@@ -7,15 +7,15 @@ import { cn, formatDate } from '@/lib/utils';
 export const BlogHeader = ({ post }: { post: Post }) => {
   return (
     <header className={cn('flex flex-col gap-4 pb-12')}>
-      <h2 className="font-heading mt-2 scroll-m-20 text-3xl font-bold leading-tight">
+      <h2 className="font-heading mt-2 scroll-m-20 text-2xl md:text-3xl font-bold leading-tight">
         {post.title}
       </h2>
-      <div className="flex items-center space-x-4">
+      <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
         <span className="text-foreground/60 font-semibold">
           {formatDate(post.date)}
         </span>
         <Separator orientation="vertical" />
-        <div className="list-none flex items-center gap-4 ml-0">
+        <div className="list-none flex items-center gap-2 ml-0">
           {post?.tags?.map((tag) => (
             <Link
               href={`/blog?tag=${tag}`}
--- src/app/projects/[...slug]/_components/project-header.tsx
+++ src/app/projects/[...slug]/_components/project-header.tsx
@@ -1,20 +1,20 @@
-import { Project } from "#site/content";
-import { ProjectTag } from "@/app/(landing)/_components/project-section/project-tag";
-import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
+import { Project } from '#site/content';
+import { ProjectTag } from '@/app/(landing)/_components/project-section/project-tag';
+import { Separator } from '@/components/ui/separator';
+import { cn } from '@/lib/utils';
 
 export const ProjectHeader = ({ project }: { project: Project }) => {
   return (
     <header className={cn('flex flex-col gap-4 pb-12')}>
-      <h2 className="font-heading mt-2 scroll-m-20 text-3xl font-bold leading-tight">
+      <h2 className="font-heading mt-2 scroll-m-20 text-2xl md:text-3xl font-bold leading-tight">
         {project.title}
       </h2>
-      <div className="flex items-center space-x-4">
+      <div className="flex flex-col md:flex-row md:items-center gap-1 md:gap-4">
         <span className="text-foreground/60 font-semibold">
           {project.period}
         </span>
         <Separator orientation="vertical" />
-        <div className="list-none flex items-center gap-4 ml-0">
+        <div className="list-none flex items-center gap-2 ml-0">
           {project.tags.map((tag) => {
             return (
               <ProjectTag key={tag.text} text={tag.text} color={tag.color} />
