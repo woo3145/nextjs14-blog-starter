f71102974b7728126a2ae3aed37531f2b385763d
woo3145 <lcwoo3145@gmail.com>
2024-05-14 21:35:38 +0900

    Fix sitemap.ts & rss.ts for i18n

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,26 +3,46 @@ import {
   docs as allDocs,
   projects as allProjects,
 } from '#site/content';
-import { siteMetadata } from '@/data/siteMetadata';
+import { host, locales, pathnames } from '@/config';
+import { getPathname } from '@/navigation';
+import { MetadataRoute } from 'next';
 
-export default async function sitemap() {
+export default function sitemap(): MetadataRoute.Sitemap {
+  const keys = Object.keys(pathnames) as Array<keyof typeof pathnames>;
+
+  function getUrl(
+    key: keyof typeof pathnames,
+    locale: (typeof locales)[number]
+  ) {
+    const pathname = getPathname({ locale, href: key });
+    return `${host}/${locale}${pathname === '/' ? '' : pathname}`;
+  }
+  const routes: {
+    url: string;
+    lastModified: Date;
+  }[] = [];
+  keys.forEach((key) => {
+    locales.forEach((locale) => {
+      routes.push({ url: getUrl(key, locale), lastModified: new Date() });
+    });
+  });
+
   const posts = allPosts.filter((post) => post.published);
   const docs = allDocs.filter((doc) => doc.published);
   const projects = allProjects.filter((doc) => doc.published);
 
   const postPages = posts.map((post) => ({
-    url: `${siteMetadata.siteUrl}/${post.slug}`,
+    url: `${host}/${post.slug}`,
+    lastModified: new Date(),
   }));
   const projectPages = projects.map((post) => ({
-    url: `${siteMetadata.siteUrl}/${post.slug}`,
+    url: `${host}/${post.slug}`,
+    lastModified: new Date(),
   }));
   const docPages = docs.map((doc) => ({
-    url: `${siteMetadata.siteUrl}/${doc.slug}`,
+    url: `${host}/${doc.slug}`,
+    lastModified: new Date(),
   }));
 
-  const routes = ['', '/blog', '/projects'].map((route) => ({
-    url: `${siteMetadata.siteUrl}${route}`,
-  }));
-
-  return [...routes, ...postPages, ...projectPages, ...docPages];
+  return [...routes, ...postPages, ...docPages, ...projectPages];
 }
