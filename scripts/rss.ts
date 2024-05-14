7f4e865e8212c2ba749ebf50f531ee0614b528dc
woo3145 <lcwoo3145@gmail.com>
2024-05-15 00:39:25 +0900

    Fix vercel build error

--- scripts/rss.ts
+++ scripts/rss.ts
@@ -1,40 +1,32 @@
 import fs from 'fs';
 import RSS from 'rss';
 import path from 'path';
-import { marked } from 'marked';
 import matter from 'gray-matter';
 import { siteMetadata } from '../data/siteMetadata';
 
 const getPosts = (locale: string): File[] => {
-  const directory = path.resolve(__dirname, `../content/${locale}/blog/`);
-  return fs
-    .readdirSync(directory)
-    .filter(
-      (file) => path.extname(file) === '.md' || path.extname(file) === '.mdx'
-    )
-    .map((file) => {
-      const postContent = fs.readFileSync(path.join(directory, file), 'utf8');
-      const { data, content }: { data: any; content: string } =
-        matter(postContent);
-      return { ...data, body: content };
-    })
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  try {
+    const directory = path.resolve(__dirname, `../content/${locale}/blog/`);
+    return fs
+      .readdirSync(directory)
+      .filter(
+        (file) => path.extname(file) === '.md' || path.extname(file) === '.mdx'
+      )
+      .map((file) => {
+        const postContent = fs.readFileSync(path.join(directory, file), 'utf8');
+        const { data, content }: { data: any; content: string } =
+          matter(postContent);
+        return { ...data, body: content };
+      })
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
 };
 
 const posts_ko = getPosts('ko');
 const posts_en = getPosts('en');
-
-const renderer = new marked.Renderer();
-renderer.link = (href, _, text) =>
-  `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
-
-marked.setOptions({
-  gfm: true,
-  breaks: true,
-  renderer,
-});
-
-const renderPost = (md: string) => marked.parse(md);
 
 const addPostsToFeed = (feed: RSS, posts: any[], locale: string) => {
   posts.forEach((post) => {
