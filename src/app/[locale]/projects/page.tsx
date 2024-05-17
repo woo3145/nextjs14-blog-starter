9eabe99b45386e592aaf8eca2524a24eaf0df973
woo3145 <lcwoo3145@gmail.com>
2024-05-18 00:13:22 +0900

    refactor: remove project/[...slug] route

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,5 +1,4 @@
-import { getSortedProjects } from '@/lib/project-utils';
-import { ProjectList } from '../(landing)/_components/project-section/project-list';
+import { ProjectSection } from '../(landing)/_components/project-section/project-section';
 
 interface ProjectPagesProps {
   params: {
@@ -9,13 +8,9 @@ interface ProjectPagesProps {
 }
 
 export default async function ProjectsPage({ params }: ProjectPagesProps) {
-  const projects = getSortedProjects(params.locale);
   return (
-    <div className="w-full">
-      <h2 className="text-center py-20 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-400">
-        프로젝트
-      </h2>
-      <ProjectList projects={projects} className="pb-20" />
+    <div className="w-full py-20">
+      <ProjectSection locale={params.locale} />
     </div>
   );
 }
