296dba87a8771ce9b0fdf1aba1d6db7292bfe5c3
woo3145 <lcwoo3145@gmail.com>
2024-05-17 22:08:51 +0900

    feat: add ContactSection in landingPage

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import { ContactSection } from './_components/contact-section';
 import { HeroSection } from './_components/hero-section';
 import { ProjectSection } from './_components/project-section/project-section';
 import { SkillSection } from './_components/skill-section/skill-section';
@@ -13,10 +14,11 @@ export default async function LandingPage({ params }: LandingPageProps) {
   return (
     <div className="flex flex-col gap-10 overflow-x-hidden">
       <HeroSection className="" />
-      <div className="py-20 px-4">
+      <div className="px-4">
         <SkillSection />
         <TechStackSection className="py-24" />
-        <ProjectSection className="pb-40" locale={params.locale} />
+        <ProjectSection locale={params.locale} />
+        <ContactSection className="py-28" />
       </div>
     </div>
   );
